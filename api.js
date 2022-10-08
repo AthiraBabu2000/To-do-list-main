6f120fe198ee86538b233caa4a2376896743ce3c
Athira Babu <athirababu9746@gmail.com>
2022-10-08 15:38:51 +0530

    to-do

--- api.js
+++ api.js
@@ -1,36 +1,81 @@
-
-
-
 function ajax(){
+
     var xhttp=new XMLHttpRequest();
-xhttp.onreadystatechange=function(){
-    if(this.readyState===4&&this.status==200){
-        var response=JSON.parse(this.responseText);
-        var output="";
-        
-        for(var i=0;i<response.length;i++){
-
-           var x = document.createElement("INPUT");
-        x.setAttribute("type", "checkbox", document.getElementById("demo").innerHTML=output);
-        output+="<li>"+response[i].title+"</li>";
-            document.body.appendChild(x);
-
-        }
-        
+    xhttp.onreadystatechange=function(){
+        if(this.readyState===4&&this.status==200){
+            var response=JSON.parse(this.responseText);
+            var output="";
            
-    }
-
-}
-xhttp.open("GET","https://jsonplaceholder.typicode.com/todos",true);
-xhttp.send();
-}
-
-function myFunction() {
-    var x = document.createElement("INPUT");
-    x.setAttribute("type", "checkbox");
-    document.body.appendChild(x);
+            for(var i=0;i<response.length;i++){
     
-  }
+                
+            output+="<li>"+response[i].title+"</li>";
+            }
+           
+            
+            document.getElementById("demo").innerHTML=output;
+        }
+    
+    }
+    xhttp.open("GET","https://jsonplaceholder.typicode.com/todos",true);
+    xhttp.send();
+
+
+
+
+
+
+let checkedCount=0;
+
+const alertPromise= ()=>{
+     return new Promise((resolve,reject)=>{
+
+         
+        if(checkedCount===5){
+            resolve(checkedCount)
+        }
+        else{
+            reject('count not equal to 5');
+        }
+    });
+}
+
+const promiseCall=()=>{
+    alertPromise().then((data)=>{
+        alert(`Congrats! ${data} Tasks have been Successfully Completed`);
+    })
+    .catch((err)=>{
+        console.log('promise rejected');
+    })
+}
+
+
+getList();
+
+$('#todoList').on('change','.checkbox',function(e){
+    if($(this).prop('checked')===true){
+        console.log('checked');
+        checkedCount++; 
+        $(this).parent().addClass('active');
+    }
+    else{
+        checkedCount--;
+        console.log('unchecked');
+        $(this).parent().removeClass('active');
+    }
+    
+    promiseCall();
+
+
+});
+}
+
+
+
+
+
+
+    
 
 
 
